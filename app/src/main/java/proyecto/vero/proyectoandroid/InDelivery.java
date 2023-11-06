a744ef7bc972493a55da14b73758493292245d0e
Veronid <verobola99@gmail.com>
2023-11-06 16:12:55 -0300

    proyecto android 6/11 actualizado

--- app/src/main/java/proyecto/vero/proyectoandroid/InDelivery.java
+++ app/src/main/java/proyecto/vero/proyectoandroid/InDelivery.java
@@ -1,14 +1,34 @@
 package proyecto.vero.proyectoandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class InDelivery extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class InDelivery extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_delivery);
+        SupportMapFragment mapFragment=(SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng DireccionMundoGelato = new LatLng(-36.603996376987, -72.09105981621634);
+        mMap.addMarker(new MarkerOptions().position(DireccionMundoGelato).title("Mundo Gelato"));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 }
